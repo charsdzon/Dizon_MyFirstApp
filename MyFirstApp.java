f09e6043f17cb37939d8062f35d1770c5e958c14
charsdzon <cmdizon@mcm.edu.ph>
2025-01-23 07:25:36 -0800

    Delete MyFirstApp.java

--- MyFirstApp.java
+++
@@ -1,8 +0,0 @@
-public class MyFirstApp {
-
-    public static void main(String[] args){
-        System.out.println("Hello my name is Charles");
-        System.out.println("The current date and time is January 23, 2025 11:03 PM");
-    }
-    
-}
