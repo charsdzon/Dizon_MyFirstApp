b5f7b651e1c1b610a0266c6574917f8248b7fbaf
charsdzon <cmdizon@mcm.edu.ph>
2025-01-27 00:12:57 +0800

    Dizon_MyFirstApp.java

--- Dizon_MyFirstApp.java
+++ Dizon_MyFirstApp.java
@@ -1,8 +1,17 @@
-public class MyFirstApp {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public static void main(String[] args){
-        System.out.println("Hello my name is Charles");
-        System.out.println("The current date and time is January 23, 2025 11:03 PM");
+public class PrintNameAndDateTime {
+    public static void main(String[] args) {
+        // Print your full name
+        String fullName = "Charles Mathew Dizon";
+        System.out.println("Full Name: " + fullName);
+
+        // Get and display the current date and time
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+
+        System.out.println("Current Date and Time: " + formattedDateTime);
     }
-    
 }
